a97737641dad6b99cd5ab0d5a0a4bf558051e552
github nagisa55 <kamomeeee00@yahoo.co.jp>
2021-03-31 15:51:11 +0900

    48

---
+++ app/controllers/guest_sessions_controller.rb
@@ -0,0 +1,8 @@
+class GuestSessionsController < ApplicationController
+  def create
+    user = User.find_by(email: "guest@example.com")
+    log_in(user)
+    flash[:success] = 'ゲストユーザーでログインしました'
+    redirect_to root_path
+  end
+end
