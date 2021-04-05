8f4a981f114b43126f115c910487ac4e6ec765fc
root <nagisa.s.0716@gmail.com>
2021-04-05 10:20:10 +0000

    55

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :message, presence: true, length: { maximum: 250 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   get "signup", to: "users#new"
+  get "contact", to: "contact#index"
+  post "contact/confirm", to: "contact#confirm"
+  post "contact/thanks", to: "contact#thanks"
 
   resources :users
 
